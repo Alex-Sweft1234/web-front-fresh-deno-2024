f7a3ddf2dfb01da62becfe0d9b225f4595c65b8e
babaev <babaev@atoms.ru>
2024-02-22 22:47:27 +0300

    fix

--- src/fresh.gen.ts
+++ src/fresh.gen.ts
@@ -4,22 +4,32 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
+import * as $_layout from "./routes/_layout.tsx";
+import * as $api_constants from "./routes/api/constants.ts";
 import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
+import * as $profile_index from "./routes/profile/index.tsx";
+import * as $start_layout from "./routes/start/_layout.tsx";
+import * as $start_index from "./routes/start/index.tsx";
+import * as $island from "./islands/island.tsx";
+import * as $page_container from "./islands/page.container.tsx";
+import * as $store_provider from "./islands/store.provider.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
+    "./routes/_layout.tsx": $_layout,
+    "./routes/api/constants.ts": $api_constants,
     "./routes/index.tsx": $index,
+    "./routes/profile/index.tsx": $profile_index,
+    "./routes/start/_layout.tsx": $start_layout,
+    "./routes/start/index.tsx": $start_index,
   },
   islands: {
-    "./islands/Counter.tsx": $Counter,
+    "./islands/island.tsx": $island,
+    "./islands/page.container.tsx": $page_container,
+    "./islands/store.provider.tsx": $store_provider,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
