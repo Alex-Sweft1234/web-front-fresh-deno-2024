f9c494f379cb4c1875641786d129ad9f429f05a8
babaev <babaev@atoms.ru>
2024-02-23 00:36:06 +0300

    fix styles

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -6,19 +6,19 @@ export default function App({ Component }: PageProps) {
   return (
     <html lang="ru">
       <head>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         
-        <meta name="description" content={`${metaData.description}`}/>
-        <meta property="og:title" content={`${metaData.titleShare}`}/>
-        <meta name="mrc__share_title" content={`${metaData.titleShare}`}/>
-        <meta property="og:description" content={`${metaData.descriptionShare}`}/>
-        <meta name="mrc__share_description" content={`${metaData.descriptionShare}`}/>
-        <meta name="theme-color" content={`${metaData.colorTheme}`}/>
+        <meta name="description" content={`${metaData.description}`} />
+        <meta property="og:title" content={`${metaData.titleShare}`} />
+        <meta name="mrc__share_title" content={`${metaData.titleShare}`} />
+        <meta property="og:description" content={`${metaData.descriptionShare}`} />
+        <meta name="mrc__share_description" content={`${metaData.descriptionShare}`} />
+        <meta name="theme-color" content={`${metaData.colorTheme}`} />
         
-        <meta property="og:image" content="/img/meta.png"/>
+        <meta property="og:image" content="/img/meta.png" />
         
-        <link rel="stylesheet" href="/styles/index.css"/>
+        <link rel="stylesheet" href="/styles/index.css" />
         <link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
         
         <title>{metaData.title}</title>
