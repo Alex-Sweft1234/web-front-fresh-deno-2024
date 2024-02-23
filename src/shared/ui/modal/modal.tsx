c9661efd9fbf37052bafecd741d0567854eb26b9
babaev <babaev@atoms.ru>
2024-02-23 16:45:14 +0300

    fix styles

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -7,12 +7,11 @@ interface IModal {
 }
 
 export function Modal({ children, isOpen = false, onClose = () => {} }: IModal) {
-  
   const onOpen = () => {}
   
   return (
-    <div data-dialog-backdrop="dialog-sm" data-dialog-backdrop-close="false" className="pointer-events-none fixed inset-0 z-[999] grid h-screen w-screen place-items-center bg-black bg-opacity-60 opacity-0 backdrop-blur-sm transition-opacity duration-300">
-      <div data-dialog="dialog-sm" className="relative m-4 w-1/3 min-w-[33.333333%] max-w-[33.333333%] rounded-lg bg-white font-sans text-base font-light leading-relaxed text-blue-gray-500 antialiased shadow-2xl">
+    <div data-dialog-backdrop="dialog" data-dialog-backdrop-close="false" className="pointer-events-none fixed inset-0 z-[999] grid h-screen w-screen place-items-center bg-black bg-opacity-60 opacity-0 backdrop-blur-sm transition-opacity duration-300">
+      <div data-dialog="dialog" className="relative m-4 w-1/3 min-w-[33.333333%] max-w-[33.333333%] rounded-lg bg-white font-sans text-base font-light leading-relaxed text-blue-gray-500 antialiased shadow-2xl">
         {children}
       </div>
     </div>
