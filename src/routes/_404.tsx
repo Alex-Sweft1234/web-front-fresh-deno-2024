4a39815dd2c853aeec1b9e48c4da53ba6b906fdb
babaev <babaev@atoms.ru>
2024-03-02 00:23:28 +0300

    fix

--- src/routes/_404.tsx
+++ src/routes/_404.tsx
@@ -5,13 +5,14 @@ export default function Error404() {
   return (
     <>
       <Head>
-        <title>404 - Page not found</title>
+        <title>404 - Страница не найдена</title>
       </Head>
       <Container className="h-[calc(100vh-310px)]">
         <div className="flex flex-col items-center justify-center h-full">
-          <h1 className="text-center text-4xl font-bold">Страница не найдена</h1>
+          <h1 className="text-center text-6xl font-bold">404</h1>
+          <h3 className="text-center text-2xl font-bold mt-5">Страница не найдена</h3>
           <div className="flex justify-center mt-10">
-            <a href="/" className="underline text-lg">На главную</a>
+            <a href="/" className="underline text-lg font-bold">На главную</a>
           </div>
         </div>
       </Container>
